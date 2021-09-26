bd6a74651090683ebc47d0b79431705ea1c5abfe
kerem <keremozfatura@me.com>
2021-09-26 17:02:31 +0300

    Initial commit

---
+++ model.py
@@ -0,0 +1,236 @@
+import abc
+import itertools
+from torch import nn
+from torch.nn import functional as F
+from torch import optim
+
+from typing import Dict, Optional, cast
+
+import numpy as np
+import torch
+from torch import distributions
+import utils as ptu
+from typing import Union
+
+
+Activation = Union[str, nn.Module]
+
+
+_str_to_activation = {
+    'relu': nn.ReLU(),
+    'tanh': nn.Tanh(),
+    'leaky_relu': nn.LeakyReLU(),
+    'sigmoid': nn.Sigmoid(),
+    'selu': nn.SELU(),
+    'softplus': nn.Softplus(),
+    'identity': nn.Identity(),
+}
+
+def build_mlp(
+        input_size: int,
+        output_size: int,
+        n_layers: int,
+        size: int,
+        activation: Activation = 'tanh',
+        output_activation: Activation = 'identity',
+):
+    """
+        Builds a feedforward neural network
+
+        arguments:
+            input_placeholder: placeholder variable for the state (batch_size, input_size)
+            scope: variable scope of the network
+
+            n_layers: number of hidden layers
+            size: dimension of each hidden layer
+            activation: activation of each hidden layer
+
+            input_size: size of the input layer
+            output_size: size of the output layer
+            output_activation: activation of the output layer
+
+        returns:
+            output_placeholder: the result of a forward pass through the hidden layers + the output layer
+    """
+    if isinstance(activation, str):
+        activation = _str_to_activation[activation]
+    if isinstance(output_activation, str):
+        output_activation = _str_to_activation[output_activation]
+    layers = []
+    in_size = input_size
+    for _ in range(n_layers):
+        layers.append(nn.Linear(in_size, size))
+        layers.append(activation)
+        in_size = size
+    layers.append(nn.Linear(in_size, output_size))
+    layers.append(output_activation)
+    return nn.Sequential(*layers)
+
+class MLPPolicy(nn.Module, metaclass=abc.ABCMeta):
+
+    def __init__(self,
+                 ac_dim,
+                 ob_dim,
+                 n_layers,
+                 size,
+                 discrete=False,
+                 learning_rate=1e-4,
+                 training=True,
+                 nn_baseline=False,
+                 **kwargs
+                 ):
+        super().__init__(**kwargs)
+
+        # init vars
+        self.ac_dim = ac_dim
+        self.ob_dim = ob_dim
+        self.n_layers = n_layers
+        self.discrete = discrete
+        self.size = size
+        self.learning_rate = learning_rate
+        self.training = training
+        self.nn_baseline = nn_baseline
+        self.baseline_loss = nn.MSELoss()
+
+        if self.discrete: # Discrete action space
+            self.logits_na = build_mlp(input_size=self.ob_dim,# NN architecture for taking action
+                                           output_size=self.ac_dim,
+                                           n_layers=self.n_layers,
+                                           size=self.size)
+            self.logits_na.to(ptu.device)
+            self.mean_net = None
+            self.logstd = None
+            self.optimizer = optim.Adam(self.logits_na.parameters(),
+                                        self.learning_rate)
+        else: # Continous action space
+            self.logits_na = None
+            self.mean_net = build_mlp(input_size=self.ob_dim,
+                                      output_size=self.ac_dim,
+                                      n_layers=self.n_layers, size=self.size) # NN for finding state based mean for the continuous action
+            self.logstd = nn.Parameter(
+                torch.zeros(self.ac_dim, dtype=torch.float32, device=ptu.device)
+            )
+            self.mean_net.to(ptu.device)
+            self.logstd.to(ptu.device)
+            self.optimizer = optim.Adam(
+                itertools.chain([self.logstd], self.mean_net.parameters()),
+                self.learning_rate
+            )
+
+        if nn_baseline: # NN architecture for baseline
+            self.baseline = build_mlp(
+                input_size=self.ob_dim,
+                output_size=1,
+                n_layers=self.n_layers,
+                size=self.size,
+            )
+            self.baseline.to(ptu.device)
+            self.baseline_optimizer = optim.Adam(
+                self.baseline.parameters(),
+                self.learning_rate,
+            )
+        else:
+            self.baseline = None
+
+    ##################################
+
+    def save(self, filepath):
+        torch.save(self.state_dict(), filepath)
+
+    ##################################
+
+    # query the policy with observation(s) to get selected action(s)
+    def get_action(self, obs: np.ndarray) -> np.ndarray: # This is part of typing module for type checking see: https://www.journaldev.com/34519/python-typing-module
+        # Obs should be np.ndarray and function should return np.ndarray
+
+        if len(obs.shape) > 1:
+            observation = obs
+        else:
+            observation = obs[None] # This works identical to the unsuqeeze method for tensor i.e., adds one more dimension
+
+        observation_tensor = torch.tensor(observation, dtype=torch.float).to(ptu.device)
+        action_distribution = self.forward(observation_tensor) # Gives the action distribution based on observation
+        return cast(
+            np.ndarray,
+            action_distribution.sample().cpu().detach().numpy(), # .numpy() ; tensor -> np.ndarray
+        )
+
+
+    # update/train this policy
+    def update(self, observations, actions, advantages, q_values=None):
+        observations = ptu.from_numpy(observations)
+        actions = ptu.from_numpy(actions)
+        advantages = ptu.from_numpy(advantages)
+
+        ################ We want to MAXIMIZE ######################
+        # is the expectation over collected trajectories of:
+        # sum_{t=0}^{T-1} [grad [log pi(a_t|s_t) * (Q_t - b_t)]]
+
+        actions_distribution = self.forward(observations)
+        # Annotates log_probs is a torch.Tensor
+        log_probs: torch.Tensor = actions_distribution.log_prob(actions)
+        if not self.discrete:  # Continuous
+            log_probs = log_probs.sum(1)
+        assert log_probs.size() == advantages.size()
+        loss = -(log_probs * advantages).sum()
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        if self.nn_baseline:
+            # Normalize the q values
+            targets = ptu.normalize(q_values, q_values.mean(), q_values.std())  # This is the real observation
+            targets = ptu.from_numpy(targets)
+            baseline_predictions: torch.Tensor = self.baseline(
+                observations).squeeze()  # This is the estimate of baseline NN
+
+            ## avoid any subtle broadcasting bugs that can arise when dealing with arrays of shape
+            ## [ N ] versus shape [ N x 1 ]
+            ## HINT: you can use `squeeze` on torch tensors to remove dimensions of size 1
+
+            assert baseline_predictions.shape == targets.shape
+
+            ####### Use mean MSE loss for baseline ########
+            baseline_loss = F.mse_loss(baseline_predictions, targets)
+
+            ####### Train baseline network
+            self.baseline_optimizer.zero_grad()
+            baseline_loss.backward()
+            self.baseline_optimizer.step()
+
+        train_log = {
+            'Training Loss': ptu.to_numpy(loss),
+        }
+        return train_log
+
+    def run_baseline_prediction(self, obs):
+        """
+            Helper function that converts `obs` to a tensor,
+            calls the forward method of the baseline MLP,
+            and returns a np array
+
+            Input: `obs`: np.ndarray of size [N, 1]
+            Output: np.ndarray of size [N]
+
+        """
+        obs = ptu.from_numpy(obs)
+        predictions = self.baseline(obs)
+        return ptu.to_numpy(predictions)[:, 0]
+
+    # This function defines the forward pass of the network.
+    # You can return anything you want, but you should be able to differentiate
+    # through it. For example, you can return a torch.FloatTensor. You can also
+    # return more flexible objects, such as a
+    # `torch.distributions.Distribution` object. It's up to you!
+    def forward(self, observation: torch.FloatTensor) -> distributions.Distribution: # The function return Distribution object
+
+        if self.discrete:
+            return distributions.Categorical(logits=self.logits_na(observation)) # Action probabilities based on logit values
+            # https://pytorch.org/docs/stable/distributions.html
+            # Creates a categorical distribution parameterized by either probs or logits
+        else:
+            assert self.logstd is not None
+            # Generate random continious action based on observation based mean and given std
+            return distributions.Normal(self.mean_net(observation),torch.exp(self.logstd)[None]) #??????????????????? Why we use [None] is the action multi dimensional ?
+            # Creates a normal (also called Gaussian) distribution parameterized by loc (mean) and scale (std).
---
+++ utils.py
@@ -0,0 +1,284 @@
+from typing import Union
+import torch
+from torch import nn
+import time
+import copy
+import os
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+
+
+
+device = None
+
+
+def init_gpu(use_gpu=True, gpu_id=0):
+    global device
+    if torch.cuda.is_available() and use_gpu:
+        device = torch.device("cuda:" + str(gpu_id))
+        print("Using GPU id {}".format(gpu_id))
+    else:
+        device = torch.device("cpu")
+        print("GPU not detected. Defaulting to CPU.")
+
+
+def set_device(gpu_id):
+    torch.cuda.set_device(gpu_id)
+
+
+def from_numpy(*args, **kwargs):
+    return torch.from_numpy(*args, **kwargs).float().to(device)
+
+
+def to_numpy(tensor):
+    return tensor.to('cpu').detach().numpy()
+
+
+def calculate_mean_prediction_error(env, action_sequence, models, data_statistics):
+    model = models[0]
+
+    # true
+    true_states = perform_actions(env, action_sequence)['observation']
+
+    # predicted
+    ob = np.expand_dims(true_states[0], 0)
+    pred_states = []
+    for ac in action_sequence:
+        pred_states.append(ob)
+        action = np.expand_dims(ac, 0)
+        ob = model.get_prediction(ob, action, data_statistics)
+    pred_states = np.squeeze(pred_states)
+
+    # mpe
+    mpe = mean_squared_error(pred_states, true_states)
+
+    return mpe, true_states, pred_states
+
+
+def perform_actions(env, actions):
+    ob = env.reset()
+    obs, acs, rewards, next_obs, terminals, image_obs = [], [], [], [], [], []
+    steps = 0
+    for ac in actions:
+        obs.append(ob)
+        acs.append(ac)
+        ob, rew, done, _ = env.step(ac)
+        # add the observation after taking a step to next_obs
+        next_obs.append(ob)
+        rewards.append(rew)
+        steps += 1
+        # If the episode ended, the corresponding terminal value is 1
+        # otherwise, it is 0
+        if done:
+            terminals.append(1)
+            break
+        else:
+            terminals.append(0)
+
+    return Path(obs, image_obs, acs, rewards, next_obs, terminals)
+
+
+def mean_squared_error(a, b):
+    return np.mean((a - b) ** 2)
+
+
+############################################
+############################################
+
+def sample_trajectory(env, policy, max_path_length, render=False, render_mode=('rgb_array')):
+    if render:
+        env.render(mode="human")
+
+    # initialize env for the beginning of a new rollout
+    ob = env.reset()
+
+    # init vars
+    obs, acs, rewards, next_obs, terminals, image_obs = [], [], [], [], [], []  # Create list of arrays
+    steps = 0
+    while True:
+        # render image of the simulated env
+        if render:
+            if 'rgb_array' in render_mode:
+                if hasattr(env,
+                           'sim'):  # The hasattr() method returns true if an object has the given named attribute and false if it does not
+                    if 'track' in env.env.model.camera_names:
+                        image_obs.append(env.sim.render(camera_name='track', height=500, width=500)[::-1])
+                    else:
+                        image_obs.append(env.sim.render(height=500, width=500)[::-1])
+                else:
+                    image_obs.append(env.render(mode=render_mode))
+            if 'human' in render_mode:
+                env.render(mode=render_mode)
+                time.sleep(env.model.opt.timestep)
+
+        # use the most recent ob to decide what to do
+        obs.append(ob)  # append the observation
+        ac = policy.get_action(ob)  # get action based on policy and the state
+        acs.append(ac)  # append the action
+
+        # take that action and record results
+        ob, rew, done, _ = env.step(ac)  # play the action
+        # done  indicates whether  the episode is finished
+
+        # record result of taking that action
+        steps += 1
+        next_obs.append(ob)  # observe the next state
+        rewards.append(rew)  # observe the result
+
+        # End the rollout if the rollout ended
+        # Note that the rollout can end due to done, or due to max_path_length
+        rollout_done = done or (steps >= max_path_length)
+        terminals.append(rollout_done)
+
+        if rollout_done:
+            break
+
+    return Path(obs, image_obs, acs, rewards, next_obs, terminals)
+
+
+def sample_trajectories(env, policy, min_timesteps_per_batch, max_path_length, render=False, render_mode=('rgb_array')):
+    timesteps_this_batch = 0
+    paths = []
+    while timesteps_this_batch < min_timesteps_per_batch:
+        paths.append(sample_trajectory(env, policy, max_path_length, render=render,
+                                       render_mode=render_mode))  # append the trajectory
+        timesteps_this_batch += get_pathlength(paths[-1])  # length of the last path (the number of observed rewards)
+    return paths, timesteps_this_batch
+
+
+def sample_n_trajectories(env, policy, ntraj, max_path_length, render=False, render_mode=('rgb_array')):
+    paths = []
+    for n in range(ntraj):
+        paths.append(sample_trajectory(env, policy, max_path_length, render, render_mode))
+
+    return paths
+
+
+############################################
+############################################
+
+def Path(obs, image_obs, acs, rewards, next_obs, terminals):
+    """
+        Take info (separate arrays) from a single rollout
+        and return it in a single dictionary
+    """
+    if image_obs != []:
+        image_obs = np.stack(image_obs, axis=0)
+    return {"observation": np.array(obs, dtype=np.float32),
+            "image_obs": np.array(image_obs, dtype=np.uint8),
+            "reward": np.array(rewards, dtype=np.float32),
+            "action": np.array(acs, dtype=np.float32),
+            "next_observation": np.array(next_obs, dtype=np.float32),
+            "terminal": np.array(terminals, dtype=np.float32)}
+
+
+def convert_listofrollouts(paths):
+    """
+        Take a list of rollout dictionaries
+        and return separate arrays,
+        where each array is a concatenation of that array from across the rollouts
+    """
+    observations = np.concatenate([path["observation"] for path in paths])
+    actions = np.concatenate([path["action"] for path in paths])
+    next_observations = np.concatenate([path["next_observation"] for path in paths])
+    terminals = np.concatenate([path["terminal"] for path in paths])
+    concatenated_rewards = np.concatenate([path["reward"] for path in paths])
+    unconcatenated_rewards = [path["reward"] for path in paths]
+    return observations, actions, next_observations, terminals, concatenated_rewards, unconcatenated_rewards
+
+
+############################################
+############################################
+
+def get_pathlength(path):
+    return len(path["reward"])
+
+
+def normalize(data, mean, std, eps=1e-8):
+    return (data - mean) / (std + eps)
+
+
+def unnormalize(data, mean, std):
+    return data * std + mean
+
+
+def add_noise(data_inp, noiseToSignal=0.01):
+    data = copy.deepcopy(data_inp)  # (num data points, dim)
+
+    # mean of data
+    mean_data = np.mean(data, axis=0)
+
+    # if mean is 0,
+    # make it 0.001 to avoid 0 issues later for dividing by std
+    mean_data[mean_data == 0] = 0.000001
+
+    # width of normal distribution to sample noise from
+    # larger magnitude number = could have larger magnitude noise
+    std_of_noise = mean_data * noiseToSignal
+    for j in range(mean_data.shape[0]):
+        data[:, j] = np.copy(data[:, j] + np.random.normal(
+            0, np.absolute(std_of_noise[j]), (data.shape[0],)))
+
+    return data
+
+class Logger:
+    def __init__(self, log_dir, n_logged_samples=10, summary_writer=None):
+        self._log_dir = log_dir
+        print('########################')
+        print('logging outputs to ', log_dir)
+        print('########################')
+        self._n_logged_samples = n_logged_samples
+        self._summ_writer = SummaryWriter(log_dir, flush_secs=1, max_queue=1)
+
+    def log_scalar(self, scalar, name, step_):
+        self._summ_writer.add_scalar('{}'.format(name), scalar, step_)
+
+    def log_scalars(self, scalar_dict, group_name, step, phase):
+        """Will log all scalars in the same plot."""
+        self._summ_writer.add_scalars('{}_{}'.format(group_name, phase), scalar_dict, step)
+
+    def log_image(self, image, name, step):
+        assert(len(image.shape) == 3)  # [C, H, W]
+        self._summ_writer.add_image('{}'.format(name), image, step)
+
+    def log_video(self, video_frames, name, step, fps=10):
+        assert len(video_frames.shape) == 5, "Need [N, T, C, H, W] input tensor for video logging!"
+        self._summ_writer.add_video('{}'.format(name), video_frames, step, fps=fps)
+
+    def log_paths_as_videos(self, paths, step, max_videos_to_save=2, fps=10, video_title='video'):
+
+        # reshape the rollouts
+        videos = [np.transpose(p['image_obs'], [0, 3, 1, 2]) for p in paths]
+
+        # max rollout length
+        max_videos_to_save = np.min([max_videos_to_save, len(videos)])
+        max_length = videos[0].shape[0]
+        for i in range(max_videos_to_save):
+            if videos[i].shape[0]>max_length:
+                max_length = videos[i].shape[0]
+
+        # pad rollouts to all be same length
+        for i in range(max_videos_to_save):
+            if videos[i].shape[0]<max_length:
+                padding = np.tile([videos[i][-1]], (max_length-videos[i].shape[0],1,1,1))
+                videos[i] = np.concatenate([videos[i], padding], 0)
+
+        # log videos to tensorboard event file
+        videos = np.stack(videos[:max_videos_to_save], 0)
+        self.log_video(videos, video_title, step, fps=fps)
+
+    def log_figures(self, figure, name, step, phase):
+        """figure: matplotlib.pyplot figure handle"""
+        assert figure.shape[0] > 0, "Figure logging requires input shape [batch x figures]!"
+        self._summ_writer.add_figure('{}_{}'.format(name, phase), figure, step)
+
+    def log_figure(self, figure, name, step, phase):
+        """figure: matplotlib.pyplot figure handle"""
+        self._summ_writer.add_figure('{}_{}'.format(name, phase), figure, step)
+
+    def dump_scalars(self, log_path=None):
+        log_path = os.path.join(self._log_dir, "scalar_data.json") if log_path is None else log_path
+        self._summ_writer.export_scalars_to_json(log_path)
+
+    def flush(self):
+        self._summ_writer.flush()
